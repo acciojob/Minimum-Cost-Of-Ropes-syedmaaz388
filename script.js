cd81d177111b4a3ba2587e4a4d74c3ebef7f5613
syedmaaz388 <63224539+syedmaaz388@users.noreply.github.com>
2023-03-25 16:19:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 function calculateMinCost() {
   //your code here
-  
-  
-  
-}  
+var res = 0;
+	var cost = 0;
+	var inputArray = document.getElementById("rope-lengths").value;
+
+	inputArray = inputArray.split(',').map(Number);
+	console.log(inputArray);
+	for(let i = 0;i< inputArray.length -1;i+=0) {
+		if (inputArray.length == 0) break;
+		inputArray = inputArray.sort((a,b) => b-a);
+		res = inputArray.pop()+ inputArray.pop();
+		cost += res;
+		inputArray[inputArray.length] = res; }
+
+	document.getElementById("result").innerHTML = cost;
+	}
